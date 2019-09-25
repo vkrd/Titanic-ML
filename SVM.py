48594038a31313d131934d9de870aa669c5accb4
Vikram <vduvvur2@illinois.edu>
2019-09-25 12:52:43 -0500

    Added SVM

---
+++ SVM.py
@@ -0,0 +1,23 @@
+from sklearn import svm
+from data_loader import *
+
+X, y = load_train_data()
+
+z = load_test_data()
+
+# discrete_predictions
+clf = svm.SVC(gamma='auto')
+clf.fit(X, y)
+
+discrete_predictions = clf.predict(z)
+
+
+# probability predictions
+clf = svm.SVC(gamma='auto', probability=True)
+clf.fit(X, y)
+
+prob_predictions = clf.predict_proba(z)
+
+# save submissions
+save_predictions(discrete_predictions, name="discrete_SVM.csv")
+save_predictions(prob_predictions, name="probability_SVM.csv")
--- data_loader.py
+++ data_loader.py
@@ -1,80 +1,177 @@
 import numpy as np
 import pandas as pd
 import re
+from sklearn.preprocessing import Imputer
 
-train = pd.read_csv("train.csv", low_memory=False)
+def load_train_data():
+    train = pd.read_csv("train.csv", low_memory=False)
 
-train = train.to_numpy()
+    train = train.to_numpy()
 
-y = train[:, 1]
+    y = train[:, 1]
 
-X = np.zeros((891, 11))
+    X = np.zeros((train.shape[0], 11))
 
-# pClass
-X[:, 0] = train[:, 2]
+    # pClass
+    X[:, 0] = train[:, 2]
 
-# Sex
-for i in range(X.shape[0]):
-    if (train[i, 4] == "male"):
-        X[i, 1] = 1
-    else:
-        X[i, 1] = 0
+    # Sex
+    for i in range(X.shape[0]):
+        if (train[i, 4] == "male"):
+            X[i, 1] = 1
+        else:
+            X[i, 1] = 0
 
-# Age
-X[:, 2] = train[:, 5]
+    # Age
+    X[:, 2] = train[:, 5]
 
-# SibSp
-X[:, 3] = train[:, 6]
+    # SibSp
+    X[:, 3] = train[:, 6]
 
-# Parch
-X[:, 4] = train[:, 7]
+    # Parch
+    X[:, 4] = train[:, 7]
 
-# Fare
-X[:, 5] = train[:, 9]
+    # Fare
+    X[:, 5] = train[:, 9]
 
-# Cabin section (assume cardinality)
-for i in range(X.shape[0]):
-    if type(train[i, 10]) == float:
-        X[i, 6] = np.nan
-    elif "A" in train[i, 10]:
-        X[i, 6] = 1
-    elif "B" in train[i, 10]:
-        X[i, 6] = 2
-    elif "C" in train[i,10]:
-        X[i, 6] = 3
-    elif "D" in train[i, 10]:
-        X[i, 6] = 4
-    elif "E" in train[i, 10]:
-        X[i, 6] = 5
-    elif "F" in train[i,10]:
-        X[i, 6] = 6
-    elif "G" in train[i, 10]:
-        X[i, 6] = 7
+    # Cabin section (assume cardinality)
+    for i in range(X.shape[0]):
+        element = train[i, 10]
 
-# Cabin number
-for i in range(X.shape[0]):
+        if type(element) == float:
+            X[i, 6] = np.nan
+        elif "A" in element:
+            X[i, 6] = 1
+        elif "B" in element:
+            X[i, 6] = 2
+        elif "C" in element:
+            X[i, 6] = 3
+        elif "D" in element:
+            X[i, 6] = 4
+        elif "E" in element:
+            X[i, 6] = 5
+        elif "F" in element:
+            X[i, 6] = 6
+        elif "G" in element:
+            X[i, 6] = 7
 
-    if type(train[i, 10]) == float:
-        X[i, 7] = np.nan
-    else:
-        #group = train[i, 10].split(" ")
-        print(len(re.findall(r'\d+', train[i, 10])))
-        if len(re.findall(r'\d+', train[i, 10])) == 0 :
+    # Cabin number
+    for i in range(X.shape[0]):
+
+        if type(train[i, 10]) == float:
             X[i, 7] = np.nan
         else:
-            X[i, 7] = int(re.findall(r'\d+', train[i, 10])[0])
+            if len(re.findall(r'\d+', train[i, 10])) == 0:
+                X[i, 7] = np.nan
+            else:
+                X[i, 7] = int(re.findall(r'\d+', train[i, 10])[0])
 
-# Embarked (one hot)
-for i in range(X.shape[0]):
-    if train[i, 11] == "Q":
-        X[i, 8] = 1
-        X[i, 9] = 0
-        X[i, 10] = 0
-    elif train[i, 11] == "C":
-        X[i, 8] = 0
-        X[i, 9] = 1
-        X[i, 10] = 0
-    elif train[i, 11] == "S":
-        X[i, 8] = 0
-        X[i, 9] = 0
-        X[i, 10] = 1
+    # Embarked (one hot)
+    for i in range(X.shape[0]):
+        if train[i, 11] == "Q":
+            X[i, 8] = 1
+            X[i, 9] = 0
+            X[i, 10] = 0
+        elif train[i, 11] == "C":
+            X[i, 8] = 0
+            X[i, 9] = 1
+            X[i, 10] = 0
+        elif train[i, 11] == "S":
+            X[i, 8] = 0
+            X[i, 9] = 0
+            X[i, 10] = 1
+
+    imp = Imputer(missing_values=np.nan, strategy='mean', axis=0)
+    imp.fit(X)
+
+    return imp.transform(X), y.astype('int')
+
+def load_test_data():
+    train = pd.read_csv("test.csv", low_memory=False)
+
+    train = train.to_numpy()
+
+    X = np.zeros((train.shape[0], 11))
+
+    # pClass
+    X[:, 0] = train[:, 1]
+
+    # Sex
+    for i in range(X.shape[0]):
+        if (train[i, 3] == "male"):
+            X[i, 1] = 1
+        else:
+            X[i, 1] = 0
+
+    # Age
+    X[:, 2] = train[:, 4]
+
+    # SibSp
+    X[:, 3] = train[:, 5]
+
+    # Parch
+    X[:, 4] = train[:, 6]
+
+    # Fare
+    X[:, 5] = train[:, 8]
+
+    # Cabin section (assume cardinality)
+    for i in range(X.shape[0]):
+        element = train[i, 9]
+        if type(element) == float:
+            X[i, 6] = np.nan
+        elif "A" in element:
+            X[i, 6] = 1
+        elif "B" in element:
+            X[i, 6] = 2
+        elif "C" in element:
+            X[i, 6] = 3
+        elif "D" in element:
+            X[i, 6] = 4
+        elif "E" in element:
+            X[i, 6] = 5
+        elif "F" in element:
+            X[i, 6] = 6
+        elif "G" in element:
+            X[i, 6] = 7
+
+    # Cabin number
+    for i in range(X.shape[0]):
+
+        if type(train[i, 9]) == float:
+            X[i, 7] = np.nan
+        else:
+            if len(re.findall(r'\d+', train[i, 9])) == 0:
+                X[i, 7] = np.nan
+            else:
+                X[i, 7] = int(re.findall(r'\d+', train[i, 9])[0])
+
+    # Embarked (one hot)
+    for i in range(X.shape[0]):
+        if train[i, 10] == "Q":
+            X[i, 8] = 1
+            X[i, 9] = 0
+            X[i, 10] = 0
+        elif train[i, 10] == "C":
+            X[i, 8] = 0
+            X[i, 9] = 1
+            X[i, 10] = 0
+        elif train[i, 10] == "S":
+            X[i, 8] = 0
+            X[i, 9] = 0
+            X[i, 10] = 1
+
+    imp = Imputer(missing_values=np.nan, strategy='mean', axis=0)
+    imp.fit(X)
+
+    return imp.transform(X)
+
+
+def save_predictions(predictions, name="submission.csv"):
+    ids = pd.read_csv("test.csv", low_memory=False).to_numpy()
+
+    ids = ids[:, 0].astype('int')
+    data = np.zeros((ids.shape[0], 2))
+    data[:, 0] = ids
+    data[:, 1] = predictions
+    np.savetxt(name, data, fmt='%i', delimiter=",")
