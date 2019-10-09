2b5b9f6c72bdd591fbee8d1a28b4d5098d930dbb
Vikram <vduvvur2@illinois.edu>
2019-10-08 22:16:41 -0500

    Neural Network fix

--- NN.py
+++ NN.py
@@ -1,5 +1,5 @@
 from data_loader import *
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 
@@ -8,30 +8,60 @@ X, y = load_train_data()
 
 z = load_test_data()
 
+sess = tf.Session(config=tf.ConfigProto(intra_op_parallelism_threads=16))
+
+# Plotting function
+def plot_history(history):
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error [MPG]')
+    plt.plot(hist['epoch'], hist['mse'],
+           label='Train Error')
+    plt.plot(hist['epoch'], hist['val_mae'],
+           label = 'Val Error')
+    plt.ylim([0,5])
+    plt.legend()
+
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Square Error [$MPG^2$]')
+    plt.plot(hist['epoch'], hist['mse'],
+           label='Train Error')
+    plt.plot(hist['epoch'], hist['val_mse'],
+           label = 'Val Error')
+    plt.ylim([0,20])
+    plt.legend()
+    plt.show()
+
+    plot_history(history)
+
 # Create model
 model = keras.Sequential([
-    keras.layers.Dense(11, input_shape=(8, )),
+    keras.layers.Dense(8, input_shape=(8, )),
     keras.layers.Dense(32, activation=tf.nn.relu),
     keras.layers.Dropout(0.5),
     keras.layers.Dense(32, activation=tf.nn.relu),
     keras.layers.Dropout(0.5),
     keras.layers.Dense(32, activation=tf.nn.relu),
     keras.layers.Dropout(0.5),
-    keras.layers.Dense(1, activation=tf.nn.softmax)
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
 ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
-              loss='binary_crossentropy',
+model.compile(optimizer='adam',
+              loss='mse',
               metrics=['accuracy'])
 
 
-history = model.fit(x=X, y=y, epochs=1000, validation_split=0.2, batch_size=32)
+history = model.fit(x=X, y=y, epochs=1500, validation_split=0.2, batch_size=32, verbose=2)
 
+# Plot metrics
+plot_history(history)
 
-# plot metrics
-pyplot.plot(history.history['val_loss'])
-pyplot.plot(history.history['loss'])
-pyplot.show()
+# Save model output
+save_predictions(model.predict(z), "NN.csv")
 
 
 model.save_weights('model_weights.h5')
