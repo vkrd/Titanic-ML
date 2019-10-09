b2a1c220f22a07203a29232f80621a69eb2d8ad8
Vikram <vduvvur2@illinois.edu>
2019-10-08 22:49:14 -0500

    More changes to NN

--- NN.py
+++ NN.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 from data_loader import *
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -8,7 +9,6 @@ X, y = load_train_data()
 
 z = load_test_data()
 
-sess = tf.Session(config=tf.ConfigProto(intra_op_parallelism_threads=16))
 
 # Plotting function
 def plot_history(history):
@@ -17,56 +17,50 @@ def plot_history(history):
 
     plt.figure()
     plt.xlabel('Epoch')
-    plt.ylabel('Mean Abs Error [MPG]')
-    plt.plot(hist['epoch'], hist['mse'],
-           label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mae'],
-           label = 'Val Error')
-    plt.ylim([0,5])
+    plt.ylabel('Binary Crossentropy Loss')
+    plt.plot(hist['epoch'], hist['binary_crossentropy'],
+             label='Train Loss')
+    plt.plot(hist['epoch'], hist['val_binary_crossentropy'],
+             label='Val Loss')
+    #plt.ylim([0, 5])
     plt.legend()
 
     plt.figure()
     plt.xlabel('Epoch')
-    plt.ylabel('Mean Square Error [$MPG^2$]')
-    plt.plot(hist['epoch'], hist['mse'],
-           label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mse'],
-           label = 'Val Error')
-    plt.ylim([0,20])
+    plt.ylabel('Hinge Loss')
+    plt.plot(hist['epoch'], hist['hinge'],
+             label='Train Error')
+    plt.plot(hist['epoch'], hist['val_hinge'],
+             label='Val Error')
+    #plt.ylim([0, 20])
     plt.legend()
     plt.show()
 
-    plot_history(history)
-
 # Create model
+
 model = keras.Sequential([
-    keras.layers.Dense(8, input_shape=(8, )),
+    keras.layers.Dense(8, input_shape=(8,)),
     keras.layers.Dense(32, activation=tf.nn.relu),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(32, activation=tf.nn.relu),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(32, activation=tf.nn.relu),
-    keras.layers.Dropout(0.5),
     keras.layers.Dense(1, activation=tf.nn.sigmoid)
 ])
 
 model.compile(optimizer='adam',
-              loss='mse',
-              metrics=['accuracy'])
+              loss='binary_crossentropy',
+              metrics=['binary_crossentropy', 'hinge'])
 
+#early_stop = keras.callbacks.EarlyStopping(monitor='binary_crossentropy', patience=20)
 
-history = model.fit(x=X, y=y, epochs=1500, validation_split=0.2, batch_size=32, verbose=2)
+history = model.fit(x=X, y=y, epochs=300, validation_split=0.2, batch_size=256, verbose=2)#, callbacks=[early_stop])
+
+# Save model output
+print(np.round(model.predict(z).flatten()[:20]))
+save_predictions(np.round(model.predict(z).flatten()), "NN.csv")
 
 # Plot metrics
 plot_history(history)
-
-# Save model output
-save_predictions(model.predict(z), "NN.csv")
-
 
 model.save_weights('model_weights.h5')
 
 # Save the model architecture
 with open('model_architecture.json', 'w') as f:
     f.write(model.to_json())
-
